779fffa2af37b643e8698a6f5d30952faa9758a2
SavIlAnd <iluxa09972@gmail.com>
2022-06-28 15:01:24 +0300

    Verify Sing

--- NearPlugin/Source.cpp
+++ NearPlugin/Source.cpp
@@ -8,12 +8,12 @@ int main()
 
     try
     {
-        //Client client("testnet");
-        //if (client.error != nullptr)
-        //{
-        //    std::cout << client.error << std::endl;
-        //}
-        //std::cout << "GetAccount: " << client.GetPublicKey() << std::endl;
+        Client client("testnet", TypeInp::REGISTRATION);
+        int res = client.VerifySing();
+        std::cout << "res: " << res << std::endl;
+        if(client.GetError() != nullptr)
+            std::cout << "GetError: " << client.GetError() << std::endl;
+
     }
     catch (const std::exception& e)
     {
